a5b699af9eecedeb2d6329f21513a7542abe911d
Eli Draluk <eli.draluk@gmail.com>
2018-02-23 17:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {'mousse':'cake'}
 
-function updateObjectWithKeyAndValue(object,key,value){
-  return Object.assign()
+function updateObjectWithKeyAndValue(obj,key,value){
+  return Object.assign({},obj,{[key]:value})
 }
